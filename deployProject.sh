b9ae9366df948a0c9c76dbaf70f47c1e48dfcaf7
许成旺 <xuchengwang@sinovatech.com>
2023-06-12 16:38:18 +0800

    调整部署脚本

--- deployProject.sh
+++ deployProject.sh
@@ -12,5 +12,5 @@ git init
 git add -A
 git commit -m $message
 git branch -M master
-git remote add origin https://github.com/prank-xcw/myblogs.git
+#git remote add origin https://github.com/prank-xcw/myblogs.git
 git push -u origin master
